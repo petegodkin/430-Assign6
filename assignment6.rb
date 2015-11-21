a2259776214643b8575818666d8f67fd8700adde
takong15 <takong15@gmail.com>
2015-11-21 14:57:42 -0800

    more recent assignment 6

---
+++ assignment6.rb
@@ -0,0 +1,127 @@
+#classes
+class ExprC
+end
+
+class Value
+end
+
+class NumC < ExprC
+  def initialize(number)
+    @number = number
+  end
+end
+
+class BoolC < ExprC
+  def initialize(boolean)
+    @boolean = boolean
+  end
+end
+
+class IdC < ExprC
+  def initialize(symbol)
+    @symbol = symbol
+  end
+end
+
+class BinopC < ExprC
+  def initialize(symbol, left, right)
+    @symbol = symbol
+    @left = left
+    @right = right
+  end
+end
+
+class IfC < ExprC
+  def initialize(expr, one, two)
+  @expr = expr
+  @one = one
+  @two = two
+  end
+end
+
+class AppC < ExprC
+  def initialize(func, args)
+    @func = func
+    @args = args
+  end
+end
+
+class LamC < ExprC
+  def initialize(params, body)
+    @params = params
+    @body = body
+  end
+end
+
+class NumV < Value
+  def initialize(number)
+    @number = number
+  end
+end
+
+class BoolV < Value
+  def initialize(boolean)
+    @boolean = boolean
+  end
+end
+
+class Binding
+  def initialize(name, val)
+    @name = name
+    @val = val
+  end
+end
+
+class CloV < Value
+  def initialize(params, body, env)
+    @params = params
+    @body = body
+    @env = env
+  end
+end
+
+#variables
+environment = []
+environment < Binding
+
+#functions
+def interp(expr, env)
+  if expr.instance_of? NumC
+    return NumV.new(expr.number)
+
+  elsif expr.instance_of? BoolC
+    return BoolV.new(expr.boolean)
+
+  elsif expr.instance_of? IdC
+    return lookup(expr.symbol, env)
+
+  elsif expr.instance_of? BinopC
+    lft = interp(expr.left, env).number
+    rght = interp(expr.right, env).number
+    return NumV.new(lft.send(expr.symbol, rght))
+
+  elsif expr.instance_of? IfC
+    condition = interp(expr.expr, env)
+    if condition.instance_of? BoolV
+      if condition.boolean == true
+        return interp(expr.one, env)
+      else
+        return interp(expr.two, env)
+      end
+    else
+      raise condition, 'Invalid Value Type in IfC in interp'
+    end
+
+  elsif expr.instance_of? LamC
+    return CloV.new(expr.params, expr.body, env)
+  end
+end
+
+def lookup(symbol, env)
+  env.each do |bind|
+    if bind.name == symbol
+      return bind.val
+    end
+  end
+  raise symbol, 'Not Found in lookup'
+end
